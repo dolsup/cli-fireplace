33aea27fc4a789f5858ec3ae86cf09c8c6252de0
choijiwon <1890mah@gmail.com>
2018-12-25 21:08:57 +0900

    maybe done yeah

---
+++ README.md
@@ -0,0 +1,24 @@
+# cli-fireplace
+
+<img src="cli-fireplace-demo.gif?raw=true">
+
+*Television, our digital fireplace*<br>
+*Christmas, It's not a biggie*
+
+## Let's get warm
+```bash
+$ npm i -g cli-fireplace
+$ cli-fireplace
+```
+
+### option
+```bash
+cli-fireplace [frame_rate]
+```
+example: `$ cli-fireplace 10`
+
+## Too laggy?
+I recommend GPU-accelerated terminal such as [alacritty](https://github.com/jwilm/alacritty)
+
+## [Original Video](https://videos.pexels.com/videos/burning-firewood-855624)
+
---
+++ index.js
@@ -0,0 +1,54 @@
+var yauzl = require('yauzl');
+var path = require('path');
+var fs = require('fs');
+var spawn = require('child_process').spawn;
+var filename = 'fireplace.yaml';
+var zippath = path.join(__dirname, 'data/fireplace.yaml.zip')
+var filepath = path.join(__dirname, filename);
+var playCmd = path.join(__dirname, 'node_modules/ascii-video-play-only/main.js');
+var frameRate = Number(process.argv[1]) || Number(process.argv[2]) || 30; // $ cli-fireplace 10 // $ node index.js 10
+
+if (typeof frameRate !== 'number' || isNaN(frameRate)) {
+    console.warn('frameRate should be a number. (default = 30)');
+    console.warn('Example: $ cli-fireplace 10');
+    process.exit(1);
+}
+
+if (fs.existsSync(filepath)) {
+    play();
+} else {
+    try {
+        // unzip video file
+        var writeStream = fs.createWriteStream(filepath);
+        console.log('building a fire...');
+        yauzl.open(zippath, function (err, zip) {
+            if (err) throw err;
+            zip.on('entry', function (entry) {
+                zip.openReadStream(entry, function (err, readStream) {
+                    if (err) throw err;
+                    readStream.pipe(writeStream);
+                    readStream.on('end', play);
+                });
+            });
+        });
+    } catch (e) {
+        console.error(e);
+        fs.unlinkSync(filepath);
+        process.exit(1);
+    }
+}
+
+function play() {
+    var fire = spawn(playCmd, ['play', filepath, '--frame_rate=' + frameRate], {
+        stdio: 'inherit',
+    });
+
+    fire.on('close', function (code, signal) {
+        if (code != 0) {
+            console.warn('The fire went out with ' + signal);
+            process.exit(code);
+        } else {
+            setImmediate(play);
+        }
+    });
+}
